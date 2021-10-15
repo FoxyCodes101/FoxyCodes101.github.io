3da40b15cca08a8d20481010f1cf6e7f956f6be3
FoxyCodes101 <70095195+FoxyCodes101@users.noreply.github.com>
2021-10-15 14:10:29 +0300

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,1 +1,27 @@
 
+<html>
+  <head>
+    <title>Foxhole</title>
+  </head>
+  <body vlink="#880000" text="#2222ff" link="#ff0000" bgcolor="#000000" alink="#00ff00">
+    <h1 style="color:red">This page is under construction<h1>
+    <form method="post">
+      <input type="text" name="inp">
+      <input type="submit" name="submit">
+    </form>
+   </body>
+</html>
+    
+
+<?php
+  if(isset($_POST['inp']))
+  {
+  $data=$_POST['inp'];
+  
+  $fp = fopen('text.txt', 'a');
+  fwrite($fp, $data);
+  fclose($fp);
+  }
+  
+
+?>
